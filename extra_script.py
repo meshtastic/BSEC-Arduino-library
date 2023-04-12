13b953d2ef616cf3a6c0c80f87a90783efa019f2
Thomas Göttgens <tgoettgens@gmail.com>
2023-04-12 09:59:49 +0200

    fix compilation for wisblock boards in meshtastic only (!)

--- extra_script.py
+++ extra_script.py
@@ -2,10 +2,11 @@ Import('env')
 from os.path import join, realpath
 
 MCU = env.get('BOARD_MCU')
-FPU = env.get('BOARD_FPU')
 
-if FPU != None and FPU != '':
-    MCU=join (MCU, FPU)
+if MCU == 'nrf52840':
+    MCU = 'cortex-m4/fpv4-sp-d16-hard'
+
+print('BSEC Software Library: MCU = ' + MCU)
 
 env.Append(
     LIBPATH=[realpath(join('src', MCU))],
