452f9a7ffa8b53e1debe2c454fe375dfad98b507
Thomas Göttgens <tgoettgens@gmail.com>
2023-04-12 10:02:13 +0200

    Fix compilation for meshtastic wisblock BSP (#1)
    * support ESP32S3
    * fix compilation for wisblock boards in meshtastic only (!)

--- extra_script.py
+++ extra_script.py
@@ -1,7 +1,14 @@
 Import('env')
 from os.path import join, realpath
 
+MCU = env.get('BOARD_MCU')
+
+if MCU == 'nrf52840':
+    MCU = 'cortex-m4/fpv4-sp-d16-hard'
+
+print('BSEC Software Library: MCU = ' + MCU)
+
 env.Append(
-    LIBPATH=[realpath(join('src', env.get('BOARD_MCU')))],
+    LIBPATH=[realpath(join('src', MCU))],
     LIBS=['algobsec']
 )
